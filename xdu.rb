bd447fee1ac8cd4a11b16cec07ffa18082551528
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-27 19:50:34 +0000

    xdu: fix homepage/url breakage
    Comments in the formula explain. Homepage/URL have been dead for, well,
    years apparently. We could probably boneyard it, but since Debian and
    Ubuntu have working links, keeping it around isn’t too odious.
    
    Closes #7.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- xdu.rb
+++ xdu.rb
@@ -1,9 +1,11 @@
-require "formula"
-
 class Xdu < Formula
-  homepage "http://sd.wareonearth.com/~phil/xdu/"
-  url "http://sd.wareonearth.com/~phil/xdu/xdu-3.0.tar.Z"
-  sha1 "196e2ba03253fd6b8a88fafe6b00e40632183d0c"
+  # Both homepage & DL-link have been dead for a long time.
+  # Switched both to Debian/Ubuntu.
+  # homepage "http://sd.wareonearth.com/~phil/xdu/"
+  homepage "https://packages.debian.org/source/unstable/xdu"
+  url "https://mirrors.kernel.org/debian/pool/main/x/xdu/xdu_3.0.orig.tar.gz"
+  mirror "http://archive.ubuntu.com/ubuntu/pool/universe/x/xdu/xdu_3.0.orig.tar.gz"
+  sha1 "960b9bdf81782d78be8d9e19908546e9acc07dcd"
 
   depends_on :x11
 
@@ -11,8 +13,9 @@ class Xdu < Formula
     ENV.append_to_cflags "-I#{MacOS::X11.include}"
     ENV.append_to_cflags "-Wno-return-type"
     ENV.append "LDFLAGS", "-L#{MacOS::X11.lib}"
+    ENV.append "LDFLAGS", "-lXaw -lXmu -lXt -lSM -lICE -lXpm -lXext -lX11"
 
-    system "#{ENV.cc} -o xdu #{ENV.cflags} #{ENV.ldflags} -lXaw -lXmu -lXt -lSM -lICE -lXpm -lXext -lX11 xdu.c xwin.c"
+    system "#{ENV.cc} -o xdu #{ENV.cflags} #{ENV.ldflags} xdu.c xwin.c"
     bin.install "xdu"
     man1.install "xdu.man" => "xdu.1x"
     (etc/"X11/app-defaults").install "XDu.ad" => "XDu"
