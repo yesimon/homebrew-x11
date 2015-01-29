b9f4dfcf12c72de345085516738843f1715b7c0f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-29 23:28:42 +0000

    zenity: add 3.14.0 bottle.

--- zenity.rb
+++ zenity.rb
@@ -3,6 +3,13 @@ class Zenity < Formula
   url "http://ftp.gnome.org/pub/gnome/sources/zenity/3.14/zenity-3.14.0.tar.xz"
   sha1 "a38b98a99c44b05c1a8573fa8e0cc53a191868fb"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/x11"
+    sha1 "3426d2077fa335d2c215cce4903741c748e578c2" => :yosemite
+    sha1 "9b356978fb0b758d0e420d3fdfa54ba538e5663b" => :mavericks
+    sha1 "aee2c119bed7d6cf1c844f9670a11b0becb806d2" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "itstool" => :build
