98bfe661e4fd5a4b57be66beaf4baed4ee3bdd1a
ojab <ojab@ojab.ru>
2015-02-09 18:16:20 +0300

    mupdf: openssl, add test and modernize
    Closes #47
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- mupdf.rb
+++ mupdf.rb
@@ -1,9 +1,8 @@
-require 'formula'
-
 class Mupdf < Formula
-  homepage 'http://mupdf.com'
-  url 'http://mupdf.com/downloads/mupdf-1.6-source.tar.gz'
-  sha1 '491d7a3b131589791c7df6dd8161c6bfe41ce74a'
+  homepage "http://mupdf.com"
+  url "http://mupdf.com/downloads/mupdf-1.6-source.tar.gz"
+  sha256 "9813ee330950a641364181649ebd369c947adbcea9d41ed953ec5365640ea563"
+  revision 1
 
   bottle do
     cellar :any
@@ -14,12 +13,21 @@ class Mupdf < Formula
 
   depends_on :macos => :snow_leopard
   depends_on :x11
+  depends_on "openssl"
+
+  conflicts_with "mupdf-tools",
+    :because => "mupdf and mupdf-tools install the same binaries."
 
   def install
     system "make", "install",
-                   "build=release",
-                   "verbose=yes",
-                   "CC=#{ENV.cc}",
-                   "prefix=#{prefix}"
+           "build=release",
+           "verbose=yes",
+           "CC=#{ENV.cc}",
+           "prefix=#{prefix}"
+  end
+
+  test do
+    pdf = test_fixtures("test.pdf")
+    assert_match /Homebrew test/, shell_output("#{bin}/mudraw -t #{pdf} 2>/dev/null")
   end
 end
