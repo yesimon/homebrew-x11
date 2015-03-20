2136aa20b14886392084114d4b07ff7e0405ff55
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-20 18:42:24 +0000

    threadscope: add 0.2.6 bottle.

--- threadscope.rb
+++ threadscope.rb
@@ -7,6 +7,13 @@ class Threadscope < Formula
   url "https://hackage.haskell.org/package/threadscope-0.2.6/threadscope-0.2.6.tar.gz"
   sha256 "ca3b3a8f57315f47f7f6787e5d92ca26c216fb67562192ae37a8cb37dceecc5f"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "ac75a07f5143b046c8390e58acf0dd10dd2bf7850a4b5602582f0f1b27ba31e7" => :yosemite
+    sha256 "adcd744ebdd49521eb0065978db916bd57b617099ce3c9fe65b236b79b411d5b" => :mavericks
+    sha256 "75e986926847ba806c046265316b518c8b8f7f14c726df60c7ede053fe82bf3f" => :mountain_lion
+  end
+
   depends_on :x11
   depends_on "cabal-install" => :build
   depends_on "pkg-config" => :build
